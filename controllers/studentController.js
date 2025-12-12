96c52e5e23a57b2e87b9b0b668eb771fa998da68
TANUJADEVIDADISETTY <dtanuja3011@gmail.com>
2025-12-12 11:41:10 +0530

    practised codes on 12/12/25

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,14 +1,20 @@
 const getStudents = (req, res) => {
     //logic
     let stdData = {name : "Tanuja", roll : "123"}; 
-    res.status(200).json({data : stdData});
+    // res.status(200).json({data : stdData});
+    res.status(200).json(stdData);
+    console.log("data added");
 }
 
 const addStudents = (req, res) => {
+    let mydata = req.body;
+    console.log(mydata);
     res.status(200).json({message : "data added"});
 }
 
 const editStudents = (req, res) => {
+    let mydata = req.body;
+    console.log(mydata);
     res.status(200).json({message : "data edited"});
 }
 
