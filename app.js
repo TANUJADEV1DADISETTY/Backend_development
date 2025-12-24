5cf8d96ca4df88212cc50b05b3486a78b588040d
TANUJADEVIDADISETTY <dtanuja3011@gmail.com>
2025-12-24 11:59:01 +0530

    post method

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import router from './routes/studentRouter.js';
-
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017").then(() => console.log("db connected"))
+mongoose.connect(
+  "mongodb+srv://dtanuja3011_db_user:Tanuja%402005@cluster0.pndakol.mongodb.net/studentDB"
+)
+.then(() => console.log("db connected"))
 .catch((error) => console.log(error));
 
 app.use('/', router);
 
 app.get('/users', (req, res) => {
-    console.log("hello this is");
     res.send("hello this is from backend");
-})
+});
 
-app.listen(5000,()=>{
-    console.log("server running at port 7007")
+app.listen(7007, () => {
+    console.log("server running at port 7007");
 });
